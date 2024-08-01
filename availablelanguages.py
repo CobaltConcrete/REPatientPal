a122a05cfb5ec06bdb1f9db910db4e21252af57c
CobaltConcrete <ganqingrong55@gmail.com>
2024-08-01 16:57:25 +0800

    Remove invalid path file

--- availablelanguages.py
+++
@@ -1,5 +0,0 @@
-from gtts import lang
-
-# List available languages
-languages = lang.tts_langs()
-print(languages)
--- playaudio.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-audio_file_path = "C:\Save Data Here\Coding stuff\Projects\REPatientPal\output.mp3"
-
-with open(audio_file_path, 'rb') as audio_file:
-    audio_base64 = base64.b64encode(audio_file.read()).decode('utf-8')
-
-print(f"AUDIO: {audio_base64}")
